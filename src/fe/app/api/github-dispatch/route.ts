6f070a7cac16b3e9f2e024a0976ec279b16133b1
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-12-15 12:42:33 +0100

    test workflow input

--- src/fe/app/api/github-dispatch/route.ts
+++ src/fe/app/api/github-dispatch/route.ts
@@ -83,7 +83,7 @@ export async function POST(request: NextRequest) {
       body: JSON.stringify({
           ref: "ansible-on-branch",
           inputs: {
-              data: body.data
+              data: JSON.stringify(body.data)
           }
       })
     });
