9faddacda182f03e38a48f1b83c87dd903d4922d
mamari90 <130982006+mamari90@users.noreply.github.com>
2026-08-24 17:09:49 +0200

    fix redis secrets

--- src/templates/common/03_redis.tf
+++ src/templates/common/03_redis.tf
@@ -79,7 +79,7 @@ resource "azurerm_key_vault_secret" "redis_{{domain_name_snake}}_hostname" {
 }
 
 resource "azurerm_key_vault_secret" "redis_{{domain_name_snake}}_connection_string" {
-  name         = "redis-${local.domain}-hostname"
+  name         = "redis-${local.domain}-connection-string"
   value        = module.redis.primary_connection_string
   key_vault_id = data.azurerm_key_vault.domain_kv.id
 }
