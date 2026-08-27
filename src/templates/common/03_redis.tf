913998cbe40c1e1c4d29d8faea32779d9b19cd75
Marco Mari <130982006+mamari90@users.noreply.github.com>
2026-08-27 15:57:37 +0200

    fix: Managed redis private dns zone (#51)

--- src/templates/common/03_redis.tf
+++ src/templates/common/03_redis.tf
@@ -22,7 +22,7 @@ module "redis" {
     enabled              = true
     vnet_name            = local.spoke_data_vnet_name
     vnet_rg_name         = local.spoke_data_vnet_resource_group_name
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_redis_cache_windows_net.id]
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_redis_azure_net.id]
   }
 
   # fixme configure the cidr list and service name allowed on this redis
